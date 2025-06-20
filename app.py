9e865fee43b30b65583321032025169a4bf8dc4f
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-20 19:47:20 -0400

    Added list of compliments

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import random
 from pynput import keyboard
+
+compliments = [
+    "Nice job, champ!",
+    "You're doing great.",
+    "Keep typing, you beast.",
+    "That was smooth typing.",
+    "The keyboard loves you.",
+    "Dang, your fingers are fast.",
+    "Code god in the making.",
+    "You're on fire!",
+    "Respect the hustle.",
+    "Typing like a legend."
+]
