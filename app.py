3031ffb9c71c7afad4abd017ffedfb31faaf09bc
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-20 19:51:47 -0400

    Detect key press function

--- app.py
+++ app.py
@@ -16,3 +16,13 @@ compliments = [
 
 key_count = 0
 trigger_limit = random.randint(20,50)
+
+def on_press(key):
+  global key_count, trigger_limit
+
+  key_count += 1
+
+  if key_count >= trigger_limit:
+    print(random.choice(compliments))
+    key_count = 0
+    trigger_limit = random.randint(20,50)
