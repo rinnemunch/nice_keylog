b89a09c5bdcd7a3a4f430250c02f24bf4dda4a22
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-21 06:35:58 -0400

    Added desktop pop-up compliments using plyer notifications

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import random
 from pynput import keyboard
+from plyer import notification
 
 compliments = [
     "Nice job, champ!",
@@ -15,22 +16,28 @@ compliments = [
 ]
 
 key_count = 0
-trigger_limit = random.randint(20,50)
+trigger_limit = random.randint(20, 50)
+
+def show_popup(message):
+    notification.notify(
+        title="Keyboard Compliment",
+        message=message,
+        timeout=3  # number of seconds
+    )
 
 def on_press(key):
-  global key_count, trigger_limit
+    global key_count, trigger_limit
+    key_count += 1
 
-  key_count += 1
-
-  if key_count >= trigger_limit:
-    print(random.choice(compliments))
-    key_count = 0
-    trigger_limit = random.randint(20,50)
+    if key_count >= trigger_limit:
+        compliment = random.choice(compliments)
+        show_popup(compliment)
+        key_count = 0
+        trigger_limit = random.randint(20, 50)
 
 def main():
-  with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
-
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 if __name__ == "__main__":
-  main()
+    main()
